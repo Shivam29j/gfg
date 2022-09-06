69f4757b01275d5c59e8fd164343594ab92bf94f
Shivam Jaiswal <jaiswalshivam373@gmail.com>
2022-09-06 12:22:00 +0530

    16

---
+++ Minimum element in a sorted and rotated array.java
@@ -0,0 +1,12 @@
+class Solution
+{
+    int findMin(int arr[], int n)
+    {
+        //complete the function here
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+    
+        }
+        return arr[0];
+    }
+}
