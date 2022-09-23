523531c21ba1ffe267012c3e24e916132f4a9b5d
Shivam Jaiswal <jaiswalshivam373@gmail.com>
2022-09-23 23:51:12 +0530

    stringrotate

---
+++ Check _if_ string _is _rotated _by _two _places.java
@@ -0,0 +1,21 @@
+class Solution
+{
+    //Function to check if a string can be obtained by rotating
+    //another string by exactly 2 places.
+    public static boolean isRotated(String str1, String str2)
+    {
+        // Your code here
+        if(str1.length()!= str2.length())   
+        return false;
+        
+        String s1 = str1.substring(2,str1.length())+str1.substring(0,2);
+        String s2= str1.substring(str1.length()-2,str1.length()) + str1.substring(0,str1.length()-2);
+        
+        if(str2.equals(s1) || str2.equals(s2))
+        return true;
+        
+        return false;
+        
+    }
+    
+}
