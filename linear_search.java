6db3971fc70aa90c88e60ae636e296e5bec27ad9
Shivam Jaiswal <jaiswalshivam373@gmail.com>
2022-10-19 23:04:36 +0530

    19

---
+++ linear_search.java
@@ -0,0 +1,9 @@
+public class LinearSearchExample{    
+public static int linearSearch(int[] arr, int key){    
+        for(int i=0;i<arr.length;i++){    
+            if(arr[i] == key){    
+                return i;    
+            }    
+        }    
+        return -1;    
+    }    
